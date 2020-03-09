aedeb2daf109ee27962721889d0788df82e93339
Austin Walela <austinmasibo@gmail.com>
2020-03-09 19:38:10 +0300

    Set up express

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 // implement your API here
-const db = require("./data/db.js")
-const express = require("express")
+const express = require('express')
+const db = require('./data/db')
+
+const PORT = 3000
 
 const app = express()
-const PORT = 4001
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "You're at the root endpoint" })
-})
-
-app.get("/hello", (req, res) => {
-  res.status(200).json({ name: "Austin Walela" })
-})
-
-app.get("/api/friends", (req, res) => {
-  console.log(req)
-  res.status(200).json({})
+app.get('/', (req, res) => {
+  console.log(req.headers)
+  res.send('Hello, World')
 })
 
 app.listen(PORT, () => {
-  console.log("Server running on port 4001")
+  console.log(`App listening on port ${PORT}!`)
 })
